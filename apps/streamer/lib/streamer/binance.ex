3cbfcdcec09821c3935faba2a89f7c4a45b84249
Oleksii Symon <usymas@gmail.com>
2024-07-21 10:41:03 +0300

    Added event json parsing

--- apps/streamer/lib/streamer/binance.ex
+++ apps/streamer/lib/streamer/binance.ex
@@ -1,6 +1,8 @@
 defmodule Streamer.Binance do
   use WebSockex
 
+  require Logger
+
   @stream_endpoint "wss://stream.binance.com:9443/ws/"
 
   def start_link(symbol) do
@@ -13,8 +15,12 @@ defmodule Streamer.Binance do
     )
   end
 
-  def handle_frame({type, msg}, state) do
-    IO.puts("Received Message - Type: #{inspect(type)} -- Message: #{inspect(msg)}")
+  def handle_frame({_type, msg}, state) do
+    case Jason.decode(msg) do
+      {:ok, event} -> Logger.info("Got event: #{event}")
+      {:error, _} -> Logger.error("Unable to parse msg: #{msg}")
+    end
+
     {:ok, state}
   end
 end
