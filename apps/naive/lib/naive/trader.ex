688b6e58813483ab41245faa4e45ddd9fed1d380
Oleksii Symon <usymas@gmail.com>
2024-07-22 10:42:56 +0300

    Added `fetch_tick_size`

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -32,4 +32,14 @@ defmodule Naive.Trader do
        tick_size: tick_size
      }}
   end
+
+  defp fetch_tick_size(symbol) do
+    Binance.get_exchange_info()
+    |> elem(1)
+    |> Map.get(:symbols)
+    |> Enum.find(&(&1["symbol"] == symbol))
+    |> Map.get("filters")
+    |> Enum.find(&(&1["filterType"] == "PRICE_FILTER"))
+    |> Map.get("tickSize")
+  end
 end
