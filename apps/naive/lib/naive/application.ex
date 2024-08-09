54af63a8c9892c9d6f9de997393086bb162d9c43
Oleksii Symon <usymas@gmail.com>
2024-08-09 11:19:42 +0300

    Added dynamic supervisor to `naive`

--- apps/naive/lib/naive/application.ex
+++ apps/naive/lib/naive/application.ex
@@ -8,12 +8,15 @@ defmodule Naive.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      {
+        DynamicSupervisor,
+        strategy: :one_for_one,
+        name: Naive.DynamicSymbolSupervisor
+      }
       # Starts a worker by calling: Naive.Worker.start_link(arg)
       # {Naive.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Naive.Supervisor]
     Supervisor.start_link(children, opts)
   end
