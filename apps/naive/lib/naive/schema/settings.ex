f0944b919e693ce7d612bfff31ea1e2c13f8f4d6
Oleksii Symon <usymas@gmail.com>
2024-08-23 11:12:24 +0300

    Added `settings` schema

---
+++ apps/naive/lib/naive/schema/settings.ex
@@ -0,0 +1,17 @@
+defmodule Naive.Schema.Settings do
+  use Ecto.Schema
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "settings" do
+    field(:symbol, :string)
+    field(:chunks, :integer)
+    field(:budget, :decimal)
+    field(:buy_down_interval, :decimal)
+    field(:profit_interval, :decimal)
+    field(:rebuy_interval, :decimal)
+    field(:status, Ecto.Enum, values: [:on, :off])
+
+    timestamps()
+  end
+end
