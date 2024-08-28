9c0548fd85ddd9d5d8e5fdfc36ac3eaed62dab75
Oleksii Symon <usymas@gmail.com>
2024-08-28 10:40:33 +0300

    Changed start_streaming

--- apps/streamer/lib/streamer.ex
+++ apps/streamer/lib/streamer.ex
@@ -3,7 +3,9 @@ defmodule Streamer do
   Documentation for `Streamer`.
   """
 
+  alias Streamer.DynamicStreamerSupervisor
+
   def start_streaming(symbol) do
-    Streamer.Binance.start_link(symbol)
+    symbol |> String.upcase() |> DynamicStreamerSupervisor.start_streaming()
   end
 end
