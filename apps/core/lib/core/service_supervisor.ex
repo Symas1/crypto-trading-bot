47c178270f4de7e69c65bdf7f0df449ed3833292
Oleksii Symon <usymas@gmail.com>
2024-10-30 09:05:57 +0000

    Updated service supervisor to be generic

--- apps/core/lib/core/service_supervisor.ex
+++ apps/core/lib/core/service_supervisor.ex
@@ -3,6 +3,65 @@ defmodule Core.ServiceSupervisor do
 
   import Ecto.Query, only: [from: 2]
 
+  defdelegate start_link(module, args, opts), to: DynamicSupervisor
+  defdelegate init(opts), to: DynamicSupervisor
+
+  defmacro __using__(opts) do
+    {:ok, repo} = Keyword.fetch(opts, :repo)
+    {:ok, schema} = Keyword.fetch(opts, :schema)
+    {:ok, module} = Keyword.fetch(opts, :module)
+    {:ok, worker_module} = Keyword.fetch(opts, :worker_module)
+
+    quote location: :keep do
+      use DynamicSupervisor
+
+      def autostart_workers do
+        Core.ServiceSupervisor.autostart_workers(
+          unquote(repo),
+          unquote(schema),
+          unquote(module),
+          unquote(worker_module)
+        )
+      end
+
+      def start_worker(symbol) do
+        Core.ServiceSupervisor.start_worker(
+          symbol,
+          unquote(repo),
+          unquote(schema),
+          unquote(module),
+          unquote(worker_module)
+        )
+      end
+
+      def stop_worker(symbol) do
+        Core.ServiceSupervisor.stop_worker(
+          symbol,
+          unquote(repo),
+          unquote(schema),
+          unquote(module),
+          unquote(worker_module)
+        )
+      end
+
+      defp get_pid(symbol) do
+        Core.ServiceSupervisor.get_pid(
+          unquote(worker_module),
+          symbol
+        )
+      end
+
+      defp update_status(symbol, status) do
+        Core.ServiceSupervisor.update_status(
+          symbol,
+          status,
+          unquote(repo),
+          unquote(schema)
+        )
+      end
+    end
+  end
+
   def autostart_workers(repo, schema, module, worker_module) do
     fetch_symbols_to_start(repo, schema)
     |> Enum.map(&start_worker(&1, repo, schema, module, worker_module))
--- apps/naive/lib/naive/dynamic_symbol_supervisor.ex
+++ apps/naive/lib/naive/dynamic_symbol_supervisor.ex
@@ -1,66 +1,35 @@
 defmodule Naive.DynamicSymbolSupervisor do
-  use DynamicSupervisor
+  use Core.ServiceSupervisor,
+    repo: Naive.Repo,
+    schema: Naive.Schema.Settings,
+    module: __MODULE__,
+    worker_module: Naive.SymbolSupervisor
 
   require Logger
 
   def start_link(init_arg) do
-    DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+    Core.ServiceSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 
-  @impl true
   def init(_init_arg) do
-    DynamicSupervisor.init(strategy: :one_for_one)
-  end
-
-  def autostart_workers() do
-    Core.ServiceSupervisor.autostart_workers(
-      Naive.Repo,
-      Naive.Schema.Settings,
-      __MODULE__,
-      Naive.SymbolSupervisor
-    )
-  end
-
-  def start_worker(symbol) do
-    Core.ServiceSupervisor.start_worker(
-      symbol,
-      Naive.Repo,
-      Naive.Schema.Settings,
-      __MODULE__,
-      Naive.SymbolSupervisor
-    )
-  end
-
-  def stop_worker(symbol) do
-    Core.ServiceSupervisor.stop_worker(
-      symbol,
-      Naive.Repo,
-      Naive.Schema.Settings,
-      __MODULE__,
-      Naive.SymbolSupervisor
-    )
+    Core.ServiceSupervisor.init(strategy: :one_for_one)
   end
 
   def shutdown_worker(symbol) when is_binary(symbol) do
     symbol = String.upcase(symbol)
 
-    case Core.ServiceSupervisor.get_pid(Naive.SymbolSupervisor, symbol) do
+    case Core.ServiceSupervisor.get_pid(symbol) do
       nil ->
         Logger.warning("#{Naive.SymbolSupervisor} worker for #{symbol} already stopped")
 
         {:ok, _settings} =
-          Core.ServiceSupervisor.update_status(symbol, :off, Naive.Repo, Naive.Schema.Settings)
+          Core.ServiceSupervisor.update_status(symbol, :off)
 
       _pid ->
         Logger.info("Shutting down #{Naive.SymbolSupervisor} worker for #{symbol}")
 
         {:ok, settings} =
-          Core.ServiceSupervisor.update_status(
-            symbol,
-            :shutdown,
-            Naive.Repo,
-            Naive.Schema.Settings
-          )
+          Core.ServiceSupervisor.update_status(symbol, :shutdown)
 
         Naive.Leader.notify(:settings_updated, settings)
         {:ok, settings}
