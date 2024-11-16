3a45e2d43da9ee39cfc0be1e48e1a2f1dc23e0d1
Oleksii Symon <usymas@gmail.com>
2024-11-16 08:59:39 +0000

    Added load from file in binance mock

--- apps/binance_mock/lib/binance_mock.ex
+++ apps/binance_mock/lib/binance_mock.ex
@@ -93,7 +93,10 @@ defmodule BinanceMock do
   end
 
   def get_exchange_info do
-    Binance.get_exchange_info()
+    case Application.get_env(:binance_mock, :use_cached_exchange_info) do
+      true -> get_cached_exchange_info()
+      _ -> Binance.get_exchange_info()
+    end
   end
 
   def order_limit_buy(symbol, quantity, price, "GTC") do
@@ -223,5 +226,22 @@ defmodule BinanceMock do
 
   defp broadcast_trade_event(%TradeEvent{} = trade_event) do
     Phoenix.PubSub.broadcast(Streamer.PubSub, "TRADE_EVENTS:#{trade_event.symbol}", trade_event)
+  end
+
+  def get_cached_exchange_info do
+    {:ok, data} =
+      File.cwd!()
+      |> Path.split()
+      |> Enum.drop(-1)
+      |> Kernel.++([
+        "binance_mock",
+        "test",
+        "assets",
+        "exchange_info.json"
+      ])
+      |> Path.join()
+      |> File.read()
+
+    {:ok, {Jason.decode!(data) |> Binance.ExchangeInfo.new()}}
   end
 end
